96df3aa2950a509aaaafad527f6c19c65b209704
Alessandro Re <ale@ale-re.net>
2017-07-19 15:36:56 +0200

    Added build tags for GPU (gccgo) and CPU (gc) codes

--- main_gpu.go
+++ main_gpu.go
@@ -1,23 +1,24 @@
-/* gsgp-go implements Geometric Semantic Genetic Programming
+// gsgp-go implements Geometric Semantic Genetic Programming
+//
+// Original C++ code from Mauro Castelli http://gsgp.sf.net
+//
+// Go port and subsequent changes from Alessandro Re
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// +build gccgo,!gc
 
-	Original C++ code from Mauro Castelli http://gsgp.sf.net
-
-	Go port and subsequent changes from Alessandro Re
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 package main
 
 // #cgo CFLAGS: -I/usr/local/cuda/include
