7264ce2706e172b59c164220b309eb6352412879
Alessandro Re <ale@ale-re.net>
2017-10-03 22:04:59 +0200

    Improved save of short runs files

--- runner/autorun/main.py
+++ runner/autorun/main.py
@@ -62,7 +62,10 @@ class Dataset:
         self._k = k
         self._written = [] # Dataset files
         self._out_path = out_dir
-        self._prepare_datasets(k, randomize, out_dir)
+
+        os.mkdir(os.path.join(out_dir, 'dataset'))
+
+        self._prepare_datasets(k, randomize)
 
     def get_fold_path(self, i):
         '''Returns the name of the i-th train/test files'''
@@ -71,10 +74,18 @@ class Dataset:
     def get_out_path(self):
         return self._out_path
 
-    def _prepare_datasets(self, k, randomize, out_path):
+    def get_train_path(self, k):
+        return f'{self._out_path}/dataset/train_{k}.dat'
+
+    def get_test_path(self, k):
+        return f'{self._out_path}/dataset/test_{k}.dat'
+
+    def _prepare_datasets(self, k, randomize):
         '''Prepare datafiles for k-fold cross validation,
         producing train_*.dat and test_*.dat files in current
         directory'''
+
+        # TODO non sarebbe male mettere i file in una directory...
 
         from random import shuffle
 
@@ -97,7 +108,7 @@ class Dataset:
         for i in range(0, dsl, size):
             j = min(dsl, i+size)
             # Path of files to write
-            train_file, test_file = f'{out_path}/train_{n}.dat', f'{out_path}/test_{n}.dat'
+            train_file, test_file = self.get_train_path(n), self.get_test_path(n)
             self._written.append((train_file, test_file))
 
             with open(train_file, 'wt') as train, open(test_file, 'wt') as test:
@@ -114,6 +125,44 @@ class Dataset:
             n += 1
 
 
+class Logger:
+    def __init__(self, basedir):
+        self._dir = basedir
+        # Create directory (assuming it's not existing)
+        os.mkdir(basedir)
+
+    def out_file_timing(self, k):
+        return f'{self._dir}/timing{k}.txt'
+
+    def out_fit_train(self, k):
+        return f'{self._dir}/fit_train_{k}.txt'
+
+    def out_fit_test(self, k):
+        return f'{self._dir}/fit_test_{k}.txt'
+    
+    def out_log_stdout(self, k):
+        return f'{self._dir}/log{k}.stdout'
+
+    def out_log_stderr(self, k):
+        return f'{self._dir}/log{k}.stderr'
+
+    def out_log_model_stderr(self, k):
+        return f'{self._dir}/mod_log{k}.stderr'
+
+    def get_mod_file(self, n, k):
+        '''Path of the mod file for combination n and cross validation k'''
+        return f'{self._dir}/mod_{n}_{k}.dat'
+
+    def open_log_stdout(self, k):
+        return open(self.out_log_stdout(k), 'at')
+
+    def open_log_stderr(self, k):
+        return open(self.out_log_stderr(k), 'at')
+
+    def open_log_model_stderr(self, k):
+        return open(self.out_log_model_stderr(k), 'at')
+
+
 class Runner:
     '''Class responsible to perform runs'''
 
@@ -124,7 +173,7 @@ class Runner:
         self._err = error_measure
         self._bin_path = bin_dir
 
-    def run(self, k, mods, n_gens):
+    def run(self, k, mods, n_gens, logger):
         '''Run the simulation for the k-th CV fold, and return '''
         ds_train, ds_test = self._ds.get_fold_path(k)
         print('Running simulation', k, 'with models', mods, 'on datasets', ds_train, ds_test, 'for', n_gens, 'generations')
@@ -132,59 +181,55 @@ class Runner:
         # The number of generations goes into config file
 
         if self._algo == 'mauro':
-            return self._run_with_temp_files(k, mods, n_gens)
+            return self._run_with_temp_files(k, mods, n_gens, logger)
         else:
-            return self._run_direct(k, mods, n_gens)
+            return self._run_direct(k, mods, n_gens, logger)
 
     def _run_with_temp_files(self, k, mods, n_gens):
         '''Runs algorithms that write to fixed paths'''
         raise NotImplementedError('Not implemented yet! Why are you not using the faster algos? :P')
 
-    def _run_direct(self, k, mods, n_gens):
+    def _run_direct(self, k, mods, n_gens, logger):
         '''Run algorithms that write to custom paths'''
 
-        outdir = self._dir
         dsdir = self._ds.get_out_path()
         bin_path = self._bin_path
         algo = 'go-gsgp-cpu'
         error_measure = self._err
-        of_timing = f'{outdir}/timing{k}.txt'
-        of_train = f'{outdir}/fit_train_{k}.txt'
-        of_test = f'{outdir}/fit_test_{k}.txt'
-        if_train = f'{dsdir}/train_{k}.dat'
-        if_test = f'{dsdir}/test_{k}.dat'
-        log_path = f'{outdir}/log{k}.'
+        if_train = self._ds.get_train_path(k)
+        if_test = self._ds.get_test_path(k)
 
         log_mode = 'wt' # We overwrite because in the end we need only the last run (FIXME?)
 
         # Run the models to get semantics
         mod_sems = []
         for n, mod in enumerate(mods):
-            mod_file = f'{outdir}/mod_{n}_{k}.dat'
-            with open(mod_file, 'w') as omod, open(log_path + 'moderr', log_mode) as merr:
+            mod_file = logger.get_mod_file(n, k)
+            with open(mod_file, 'w') as omod, logger.open_log_model_stderr(k) as merr:
                 subprocess.run([mod, if_train, if_test], stdout=omod, stderr=merr)
             mod_sems.append(mod_file)
 
         run_args = [f'{bin_path}/{algo}',
             '-error_measure', error_measure,
-            '-out_file_exec_timing', of_timing,
-            '-out_file_train_fitness', of_train,
-            '-out_file_test_fitness', of_test,
+            '-out_file_exec_timing', logger.out_file_timing(k),
+            '-out_file_train_fitness', logger.out_fit_train(k),
+            '-out_file_test_fitness', logger.out_fit_test(k),
             '-train_file', if_train,
             '-test_file', if_test,
             '-max_number_generations', n_gens,
             *mod_sems,
             ]
         #print('RUNNING', f'{bin_path}/{algo}', *run_args, '2>', log_path)
-        with open(log_path + 'stdout', log_mode) as lout, open(log_path + 'stderr', log_mode) as lerr:
+        with logger.open_log_stdout(k) as lout, logger.open_log_stderr(k) as lerr:
             subprocess.run([str(a) for a in run_args], stdout=lout, stderr=lerr)
 
-        with open(of_train) as ftrain:
+        # Get the last fitness value
+        with open(logger.out_fit_train(k)) as ftrain:
             last_line = None
             for row in ftrain:
                 last_line = row
             train_fit = float(last_line)
-        with open(of_test) as tfit:
+        with open(logger.out_fit_test(k)) as tfit:
             last_line = None
             for row in tfit:
                 last_line = row
@@ -198,6 +243,9 @@ def run_sim(args, dataset, out_dir):
 
     # Prepare for running
     runner = Runner(algo, dataset, out_dir=out_dir, bin_dir=args.bindir)
+
+    logger_selection = Logger(out_dir + '/selection')
+    logger_longrun = Logger(out_dir + '/longrun')
 
     # Where fitnesses are saved for CV
     cv_fits = []
@@ -218,7 +266,7 @@ def run_sim(args, dataset, out_dir):
             # For every combination of models
             for mods in models2:
                 # Run simulation gathering results
-                train_fit, test_fit = runner.run(k, mods, args.shortg)
+                train_fit, test_fit = runner.run(k, mods, args.shortg, logger_selection)
                 # Accumulate fitnesses for this fold
                 k_fits.append((train_fit, test_fit))
             # Accumulate fitnesses for cross validation
@@ -239,7 +287,7 @@ def run_sim(args, dataset, out_dir):
     k_fits = []
     for k in range(args.k_fold):
         #ds_train, ds_test = dataset.get_fold_path(k)
-        train_fit, test_fit = runner.run(k, best_models, args.longg)
+        train_fit, test_fit = runner.run(k, best_models, args.longg, logger_longrun)
         k_fits.append((train_fit, test_fit))
 
     print(cv_average(k_fits))
