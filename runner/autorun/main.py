b0d660ce71e142242b435690cc977e621c192d1e
Alessandro Re <ale@ale-re.net>
2017-10-18 21:13:31 +0200

    General improvements in autorun

--- runner/autorun/main.py
+++ runner/autorun/main.py
@@ -1,53 +1,42 @@
 #!/usr/bin/env python3
 
+"""Run simulations with various options."""
+
 import os
+import time
 import pickle
 import shutil
+import logging
 import argparse
 import subprocess
 import numpy as np
+from collections import Counter
+
+
+global_stats = dict()
+
+
+def logi(log, message):
+    """Log the message in the specified log."""
+    logging.getLogger(log).info(message)
 
 
 def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)."
     from itertools import chain, combinations
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 
-def split(dataset, k):
-    '''Returns two file names containing the k-th fold of cross-validation'''
-    return 'foo{} bar{}'.format(k, k).split()
-
-
-def select_models(selected, models):
-    return models[selected] # return [models[i] for i in selected]
-
-
 def cv_average(cv_fits): 
+    """Compute average for CV fitness results."""
     fits = np.array(cv_fits) # Convert to numpy array
     return np.mean(fits, axis=0) # Compute average across CV runs
 
 
-def old_best_cv(cv_fits):
-    '''Given a cv_fits, computes the average for the CV process,
-    then determines which models will to be used using the test
-    fitness results'''
-
-    average = cv_average(cv_fits)
-    tf = average[:,1] # Take only test fitnesses
-    nomax = tf[tf < np.percentile(tf, 75)] # Exclude large values
-    # Compute average and standard deviation
-    avg = np.mean(nomax)
-    std = np.std(nomax)
-    # Return indices of the values within 2 std
-    usable = tf < avg + 2 * std
-    return [i for i, b in enumerate(usable.data) if b]
-
-
 def best_cv(cv_fits):
     '''Given a cv_fits, computes the average for the CV process,
-    then determines which models will to be used using the test
+    `then determines which models will to be used using the test
     fitness results. Selects the best of the combinations used'''
 
     average = cv_average(cv_fits)
@@ -75,17 +64,15 @@ class Dataset:
         return self._out_path
 
     def get_train_path(self, k):
-        return f'{self._out_path}/dataset/train_{k}.dat'
+        return os.path.join(self._out_path, 'dataset', f'train_{k}.dat')
 
     def get_test_path(self, k):
-        return f'{self._out_path}/dataset/test_{k}.dat'
+        return os.path.join(self._out_path, 'dataset', f'test_{k}.dat')
 
     def _prepare_datasets(self, k, randomize):
         '''Prepare datafiles for k-fold cross validation,
         producing train_*.dat and test_*.dat files in current
         directory'''
-
-        # TODO non sarebbe male mettere i file in una directory...
 
         from random import shuffle
 
@@ -132,26 +119,26 @@ class Logger:
         os.mkdir(basedir)
 
     def out_file_timing(self, k):
-        return f'{self._dir}/timing{k}.txt'
+        return os.path.join(self._dir, f'timing{k}.txt')
 
     def out_fit_train(self, k):
-        return f'{self._dir}/fit_train_{k}.txt'
+        return os.path.join(self._dir, f'fit_train_{k}.txt')
 
     def out_fit_test(self, k):
-        return f'{self._dir}/fit_test_{k}.txt'
+        return os.path.join(self._dir, f'fit_test_{k}.txt')
     
     def out_log_stdout(self, k):
-        return f'{self._dir}/log{k}.stdout'
+        return os.path.join(self._dir, f'log{k}.stdout')
 
     def out_log_stderr(self, k):
-        return f'{self._dir}/log{k}.stderr'
+        return os.path.join(self._dir, f'log{k}.stderr')
 
     def out_log_model_stderr(self, k):
-        return f'{self._dir}/mod_log{k}.stderr'
+        return os.path.join(self._dir, f'mod_log{k}.stderr')
 
     def get_mod_file(self, n, k):
         '''Path of the mod file for combination n and cross validation k'''
-        return f'{self._dir}/mod_{n}_{k}.dat'
+        return os.path.join(self._dir, f'mod_{n}_{k}.dat')
 
     def open_log_stdout(self, k):
         return open(self.out_log_stdout(k), 'at')
@@ -193,9 +180,6 @@ class Runner:
         '''Run algorithms that write to custom paths'''
 
         dsdir = self._ds.get_out_path()
-        bin_path = self._bin_path
-        algo = 'go-gsgp-cpu'
-        error_measure = self._err
         if_train = self._ds.get_train_path(k)
         if_test = self._ds.get_test_path(k)
 
@@ -209,8 +193,8 @@ class Runner:
                 subprocess.run([mod, if_train, if_test], stdout=omod, stderr=merr)
             mod_sems.append(mod_file)
 
-        run_args = [f'{bin_path}/{algo}',
-            '-error_measure', error_measure,
+        run_args = [os.path.join(self._bin_path, self._algo),
+            '-error_measure', self._err,
             '-out_file_exec_timing', logger.out_file_timing(k),
             '-out_file_train_fitness', logger.out_fit_train(k),
             '-out_file_test_fitness', logger.out_fit_test(k),
@@ -239,49 +223,60 @@ class Runner:
 
 def run_sim(args, dataset, out_dir):
     # A friendly reminder
-    print('We are going to run', args.k_fold * 2**len(models), 'times the short version and save output to', out_dir)
+    logger_other.info(f'We are going to run {args.k_fold * 2**len(models)} times the short version and save output to {out_dir}')
 
     # Prepare for running
-    runner = Runner(algo, dataset, out_dir=out_dir, bin_dir=args.bindir)
+    runner = Runner(args.algorithm, dataset, out_dir=out_dir, bin_dir=args.bindir)
 
-    logger_selection = Logger(out_dir + '/selection')
-    logger_longrun = Logger(out_dir + '/longrun')
+    logger_selection = Logger(os.path.join(out_dir, 'selection'))
+    logger_longrun = Logger(os.path.join(out_dir, 'longrun'))
 
     # Where fitnesses are saved for CV
     cv_fits = []
 
-    models2 = list(powerset(models))
-
-    if False:
-        #with open('salvato_risultati', 'rb') as fpi:
-        #    cv_fits = pickle.load(fpi)
-        pass
+    if args.all:
+        best_models = models2[-1] # Use all models 
+        global_stats['sel_time'] = global_stats.get('sel_time', 0)
     else:
-        # Perform k-fold cross validation
-        for k in range(args.k_fold):
-            ## Split the dataset and get two file names
-            #ds_train, ds_test = dataset.get_fold_path(k)
-            # Where fitnesses are saved for this fold
-            k_fits = []
-            # For every combination of models
-            for mods in models2:
-                # Run simulation gathering results
-                train_fit, test_fit = runner.run(k, mods, args.shortg, logger_selection)
-                # Accumulate fitnesses for this fold
-                k_fits.append((train_fit, test_fit))
-            # Accumulate fitnesses for cross validation
-            cv_fits.append(k_fits)
+        if False:
+            #with open('salvato_risultati', 'rb') as fpi:
+            #    cv_fits = pickle.load(fpi)
+            pass
+        else:
+            t_start = time.perf_counter()
+            # Perform k-fold cross validation
+            for k in range(args.k_fold):
+                ## Split the dataset and get two file names
+                #ds_train, ds_test = dataset.get_fold_path(k)
+                # Where fitnesses are saved for this fold
+                k_fits = []
+                # For every combination of models
+                for mods in models2:
+                    # Run simulation gathering results
+                    train_fit, test_fit = runner.run(k, mods, args.shortg, logger_selection)
+                    # Accumulate fitnesses for this fold
+                    k_fits.append((train_fit, test_fit))
+                # Accumulate fitnesses for cross validation
+                cv_fits.append(k_fits)
+            t_tot = time.perf_counter() - t_start
+            logi('stats.selection.walltimes', f'Time for running selection: {t_tot}')
+            global_stats['sel_time'] = global_stats.get('sel_time', 0) + t_tot
 
-        # Save for convenience
-        #with open('salvato_risultati', 'wb') as fpo:
-        #    pickle.dump(cv_fits, fpo)
+            # Save for convenience
+            #with open('salvato_risultati', 'wb') as fpo:
+            #    pickle.dump(cv_fits, fpo)
 
-    # FIXME these messages should go on the log file :\
-    print('Average fitnesses of CV tests (models combinations on rows)')
-    print(cv_average(cv_fits))
+        logi('stats.selection.cv.fitness.average', f'Average fitnesses of CV tests (models combinations on rows)\n{cv_average(cv_fits)}')
 
-    # Compute cross validation
-    best_models = select_models(best_cv(cv_fits), models2)
+        t_start = time.perf_counter()
+        # Compute cross validation
+        bm = best_cv(cv_fits)
+        best_models = models2[bm]
+
+        logi('stats.selection.models.best', f'{bm} {best_models}')
+        global_stats['best_models'] = global_stats.get('best_models', Counter()) + Counter({bm: 1})
+
+    # TODO testare l'argomento --all
 
     # Perform long run, using only selected models
     k_fits = []
@@ -290,38 +285,45 @@ def run_sim(args, dataset, out_dir):
         train_fit, test_fit = runner.run(k, best_models, args.longg, logger_longrun)
         k_fits.append((train_fit, test_fit))
 
-    print(cv_average(k_fits))
-
+    logi('stats.longrun.cv.fitness.average', f'Average CV: {cv_average(k_fits)}')
+    t_tot = time.perf_counter() - t_start
+    logi('stats.longrun.walltimes', f'Total time for longruns: {t_tot}')
+    global_stats['lon_time'] = global_stats.get('lon_time', 0) + t_tot
 
 if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser(description='Run tests with CV model selection')
+    parser.add_argument('--all', type=bool, default=False, help='Use all models without selection')
     parser.add_argument('--k_fold', '-k', type=int, default=10, help='Number of folds')
     parser.add_argument('--runs', '-r', type=int, default=30, help='Number of runs')
     parser.add_argument('--config', '-C', type=str, default=None, help='Configuration file to use')
     parser.add_argument('--bindir', '-B', type=str, default='..', help='Directory containing binaries')
     parser.add_argument('--shortg', '-s', type=int, default=100, help='Number of generations for short runs')
     parser.add_argument('--longg', '-l', type=int, default=1000, help='Number of generations for long runs')
+    parser.add_argument('--algorithm', '-A', type=str, default='go-gsgp-cpu', help='Path of algo to use')
     parser.add_argument('datafile', type=open, help='Dataset file to load')
+    #parser.add_argument('modeldir', type=str, help='Models directory')
     parser.add_argument('outdir', type=str, help='Output directory')
     args = parser.parse_args()
 
     # Models we are applying
     models = ['../models/nl_lr_sem.py', '../models/nl_mlp_sem.py', '../models/nl_svr_sem.py']
-    # Full dataset
-    #datafile = args.datafile # './dataset.txt'
-    # Algorithm to use
-    algo = '../go-gsgp-cpu'
+    models2 = list(powerset(models))
 
     # Create root directory for all the results
     os.mkdir(args.outdir)
 
+    logging.basicConfig(filename=os.path.join(args.outdir, 'stats.log'), level=logging.INFO)
+
+    logger_stats = logging.getLogger('stats')
+    logger_other = logging.getLogger('other')
+
     # If provided, copy configuration file
     if args.config is not None:
-        cfg = f'{args.outdir}/configuration.ini'
+        cfg = os.path.join(args.outdir, 'configuration.ini')
         shutil.copy(args.config, cfg)
         args.config = cfg # Replace old choice
-        print(f'This is the moment to review your configuration file in {cfg}')
+        print(f'This is the time to review your configuration file in {cfg}')
         print(f'Generation counts will be {args.shortg} (short) and {args.longg} (long)')
         input(f'Press Enter when ready to go.')
 
@@ -331,9 +333,13 @@ if __name__ == '__main__':
     for r in range(args.runs):
         # FIXME one log per run, then use log module
         # FIXME this breaks when using paths like '../somedir'
-        outdir = f'{args.outdir}/{args.outdir}_{r}'
+        outdir = os.path.join(args.outdir, os.path.basename(args.outdir) + str(r))
         # Create output directory
         os.mkdir(outdir)
         # Run algo
         run_sim(args, dataset, outdir)
+
+    logi('stats.walltimes', f'Total selection and longrun wallclock time: {global_stats["sel_time"]} {global_stats["lon_time"]}')
+    logi('stats.counters', f'')
+    logi('stats.selection.models.frequency', f'{global_stats["best_models"]}')
 
